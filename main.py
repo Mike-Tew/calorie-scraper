f8651e35c411bac07c761bfbd78d6dd2682759cb
Michael Tew <mike.tew@gmail.com>
2022-02-07 11:51:38 -0800

    Implemented threading.

--- main.py
+++ main.py
@@ -3,29 +3,16 @@
 # Scrape calorie data
 # Calculate data
 
+import requests
+import threading
 from tkinter import END, Tk, LabelFrame, Label, Button
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
-import requests
 
 
 DAYS_TO_SCRAPE = 5
 calories_list = []
 
-# for day in range(DAYS_TO_SCRAPE, 0, -1):
-#     date = datetime.today() - timedelta(days=day)
-#     formatted_date = date.strftime("%Y-%m-%d")
-
-#     page = requests.get(
-#         f"https://www.myfitnesspal.com/food/diary/Switesir?date={formatted_date}"
-#     )
-#     soup = BeautifulSoup(page.content, "html.parser")
-#     calories = soup.find("tr", class_="remaining").contents[3].get_text()
-
-#     format_calories = int(calories.replace(",", ""))
-#     calories_list.append(format_calories)
-
-#     print(calories_list)
 
 # prev_list = [500, 700, 1001, 1050, 875]
 
@@ -40,11 +27,30 @@ class Gui(Tk):
         self.calorie_frame.pack()
         self.text_box = Label(self.calorie_frame)
         self.text_box.pack()
-        self.scrape_button = Button(self.calorie_frame, text="Scrape", command=self.scrape)
+        self.scrape_button = Button(
+            self.calorie_frame,
+            text="Scrape",
+            command=threading.Thread(target=self.scrape).start
+        )
         self.scrape_button.pack()
 
     def scrape(self):
         self.text_box["text"] = "Calories"
+        for day in range(DAYS_TO_SCRAPE, 0, -1):
+            date = datetime.today() - timedelta(days=day)
+            formatted_date = date.strftime("%Y-%m-%d")
+
+            page = requests.get(
+                f"https://www.myfitnesspal.com/food/diary/Switesir?date={formatted_date}"
+            )
+            soup = BeautifulSoup(page.content, "html.parser")
+            calories = soup.find("tr", class_="remaining").contents[3].get_text()
+
+            format_calories = int(calories.replace(",", ""))
+            calories_list.append(format_calories)
+            print(format_calories)
+
+        print(calories_list)
 
 
 if __name__ == "__main__":
